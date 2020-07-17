b21928e03bb0766eb5b1c5f315a633ea5959cf26
bryanbuckus <bryanbuckus@gmail.com>
2020-07-17 14:04:47 -0400

    added logic for the spaces and underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,22 @@
 const hangNames= ["hulk", "iron man", "thor", "captain america"];
 
 // next we need the application to randomly choose one of the terms
-
+//  generates a number with the corresponding terms 
 randomHangName = Math.floor(Math.random() * hangNames.length);
 console.log(randomHangName);
-
+// grabs one of the names from the array
 gameName = hangNames[randomHangName];
 console.log(gameName);
+// turns selected name into individual letters
+lettersInName = gameName.split("");
+console.log(lettersInName);
+// start adding underscores and spaces for names
+let underscores = [];
+for (var i = 0;i < lettersInName.length; i++ ) {
+ if (lettersInName[i] === (" ")){
+    underscores.push("*");
+ } else {
+     underscores.push("_");
+ }
+};
+console.log(underscores);
