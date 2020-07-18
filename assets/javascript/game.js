e56a7f706709967f0a8aa9f1f5b4f29ff2c21294
bryanbuckus <bryanbuckus@gmail.com>
2020-07-18 12:19:29 -0400

    game is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,15 @@
+
+let numberOfGuesses;
+let wins = 0;
+let losses = 0;
+let lettersGuessed = [];
+
 function startGame () {
 // first we need a list of potential words for our hang man game
 const hangNames= ["hulk", "ironman", "thor", "captainamerica"];
 // some variables for keeping track of guesses
 
-let lettersGuessed = [];
+lettersGuessed = [];
 // next we need the application to randomly choose one of the terms
 //  generates a number with the corresponding terms 
 randomHangName = hangNames[Math.floor(Math.random() * hangNames.length)];
@@ -14,6 +20,7 @@ console.log(lettersInName);
 // start adding underscores and spaces for names
 underscores = lettersInName.length
 console.log(underscores);
+numberOfGuesses = underscores + 2;
 
 numOfSpaces = [];
 for (i = 0; i < underscores; i++) {
@@ -22,7 +29,10 @@ numOfSpaces.push("_");
 console.log(numOfSpaces);
 
 document.getElementById("random-word").innerHTML = numOfSpaces.join(" ");
-document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(" ");
+document.getElementById("letters-guessed").innerHTML = "Letter Guessed: " + lettersGuessed.join(" ");
+document.getElementById("guesses-remaining").innerHTML = "Number of Guesses Remaining: " + numberOfGuesses;
+document.getElementById("wins").innerHTML = "Wins: " + wins;
+document.getElementById("losses").innerHTML = "Losses: " + losses;
 };
 
 function checkLetters(letter) {
@@ -38,19 +48,38 @@ function checkLetters(letter) {
                 numOfSpaces[j] = letter;
             };
         };
-    };
+    } else {
+        numberOfGuesses--;
+    }
 }
 // targeting paragraph id to print to screen
 
 function endRound () {
-    document.getElementById("random-word").innerHTML = numOfSpaces.join(" ");
-document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(" ");
+document.getElementById("random-word").innerHTML = numOfSpaces.join(" ");
+document.getElementById("letters-guessed").innerHTML = "Letters Guessed: " + lettersGuessed.join(" ");
+document.getElementById("guesses-remaining").innerHTML = "Number of Guesses Remaining: " + numberOfGuesses;
+document.getElementById("wins").innerHTML = "Wins: " + wins;
+document.getElementById("losses").innerHTML = "Losses: " + losses;
+checkWin();
+
+};
+function checkWin() {
+    if (numOfSpaces.join("") === randomHangName) {
+        alert("you win");
+        wins++;
+        startGame();
+    }else if (numberOfGuesses === 0){
+        alert("game over");
+        losses++;
+        startGame();
+    }
 }
+
 
 
 startGame();
 // empty array for guessed letters
-lettersGuessed = []
+// lettersGuessed = []
 // user clicks a key to guess a letter
  document.onkeyup = function (event) {
      
@@ -58,11 +87,18 @@ lettersGuessed = []
     if (event.keyCode >= 65 && event.keyCode <= 90) {
      let userGuess = event.key;
      console.log(userGuess);
+     for (k = 0; k < lettersGuessed.length; k++){
+        if (lettersGuessed[k] === userGuess) {
+            alert("you already guessed that letter");
+            return;
+        }
+    }
      lettersGuessed.push(userGuess);
      console.log(lettersGuessed);
      
      checkLetters(userGuess);
      endRound();
+     
 
      }else {
          alert("only use letters")
